7437682b3e47a2c87e2d8f33d656ea0f3da683cc
Artur Prepelytsia <preart81@gmail.com>
2025-05-01 00:16:23 +0300

    Оновлено модель категорії з main

--- db/models/category.js
+++ db/models/category.js
@@ -1,26 +1,18 @@
 import { DataTypes } from "sequelize";
 import sequelize from "../Sequelize.js";
 
-const Category = sequelize.define(
-  "category",
-  {
-    _id: {
-      type: DataTypes.STRING,
-      primaryKey: true,
-      allowNull: false,
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-    },
+const Category = sequelize.define("category", {
+  _id: {
+    type: DataTypes.STRING,
+    primaryKey: true,
   },
-  {
-    timestamps: true,
-  }
-);
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+});
 
-// Синхронізуємо модель з базою даних
-Category.sync();
+// Category.sync();
 
 export default Category;
