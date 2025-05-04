5f173673d176408535a5b60bcb16657d1d58e64a
Yevhen Nesvit <yevhen.nesvit@gmail.com>
2025-05-04 12:15:04 +0300

    add path import

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,6 +1,7 @@
 import multer from 'multer';
 
 import fs from 'fs/promises';
+import path from 'path';
 
 import { v2 as cloudinary } from 'cloudinary';
 import { CloudinaryStorage } from 'multer-storage-cloudinary';
