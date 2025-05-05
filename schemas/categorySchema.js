19dfffc9b725cfa838a51dc708fa4943a131552a
akolvakh <a.kolvakh@outlook.com>
2025-05-05 15:49:54 +0300

    #comment ANTON update category schema

--- schemas/categorySchema.js
+++ schemas/categorySchema.js
@@ -8,6 +8,10 @@ const createCategorySchema = Joi.object({
   description: Joi.string().optional().messages({
     'string.empty': 'Description cannot be empty',
   }),
+  thumb: Joi.string().uri().optional().messages({
+    'string.uri': 'Thumb must be a valid URL',
+    'string.empty': 'Thumb cannot be empty',
+  }),
 });
 
 export { createCategorySchema };
